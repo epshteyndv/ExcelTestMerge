c38a2dc66c209aaf5eead25a091716b1ee5a7253
Denis Epshteyn <denis.epshteyn@9bit.ru>
2016-02-20 18:04:27 +0600

    ini

---
+++ ExcelTestMerge/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ExcelTestMerge
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.selectFolderButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.saveTestingTemplateDialog = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog
+            // 
+            this.folderBrowserDialog.ShowNewFolderButton = false;
+            this.folderBrowserDialog.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // selectFolderButton
+            // 
+            this.selectFolderButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.selectFolderButton.Location = new System.Drawing.Point(12, 12);
+            this.selectFolderButton.Name = "selectFolderButton";
+            this.selectFolderButton.Size = new System.Drawing.Size(310, 23);
+            this.selectFolderButton.TabIndex = 0;
+            this.selectFolderButton.Text = "Выбор папки";
+            this.selectFolderButton.UseVisualStyleBackColor = true;
+            this.selectFolderButton.Click += new System.EventHandler(this.selectFolderButton_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(12, 40);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(310, 23);
+            this.progressBar.TabIndex = 1;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(127, 89);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(195, 13);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Сохранить шаблон для тестирования";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // saveTestingTemplateDialog
+            // 
+            this.saveTestingTemplateDialog.FileName = "Шабон_для_теста.xlsx";
+            this.saveTestingTemplateDialog.Filter = "Excel FIle|*.xlsx";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 111);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.selectFolderButton);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(350, 150);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(350, 150);
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Сбор результатов тестов";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button selectFolderButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.SaveFileDialog saveTestingTemplateDialog;
+    }
+}
+
---
+++ ExcelTestMerge/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+
+namespace ExcelTestMerge
+{
+    public partial class mainForm : Form
+    {
+        private const string ResultXlsx = "!Результат.xlsx";
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+            
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectFolderButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            var testFilesFolder = new DirectoryInfo(folderBrowserDialog.SelectedPath);
+            var resultFilePath = Path.Combine(folderBrowserDialog.SelectedPath, ResultXlsx);
+
+            var testcheckXlsx = File.Exists(resultFilePath) ? resultFilePath : "TestCheck.xlsx";
+            var xlWorkbook = new XLWorkbook(testcheckXlsx);
+            var data = xlWorkbook.Worksheet("Данные");
+
+            data.Range($"B3", $"BJ102").Clear(XLClearOptions.Contents);
+            
+            var testFiles = testFilesFolder.GetFiles("*.xlsx").ToList();
+            testFiles.RemoveAll(f => f.Name.Equals(ResultXlsx, StringComparison.InvariantCultureIgnoreCase));
+
+            progressBar.Maximum = testFiles.Count;
+            progressBar.Value = 0;
+
+            var startIndex = 3;
+
+            var list = new List<TestData>();
+
+            foreach (var testFile in testFiles)
+            {
+                try
+                {
+                    list.Add(GetData(testFile));
+                }
+                catch (Exception)
+                {
+                }
+                
+                progressBar.Value++;
+            }
+
+            foreach (var testData in list.OrderBy(t => t.Student))
+            {
+                data.Cell($"B{startIndex}").Value = testData.Student;
+
+                var cells = data.Range($"C{startIndex}", $"BJ{startIndex}").Cells().ToList();
+
+                for (var i = 0; i < cells.Count; i++)
+                    cells[i].SetValue(testData.Values[i]);
+
+                startIndex++;
+            }
+
+            
+            xlWorkbook.SaveAs(resultFilePath);
+
+            System.Diagnostics.Process.Start(resultFilePath);
+        }
+
+        private TestData GetData(FileInfo file)
+        {
+            var testData = new TestData();
+
+            var testDataBook = new XLWorkbook(file.FullName);
+            var testSheet = testDataBook.Worksheet("Тест");
+
+            testData.Student = testSheet.Cell("F1").GetValue<string>();
+            
+            testData.Values.AddRange(testSheet.Range("B4", "U4").Cells().Select(c => c.GetValue<string>()));
+            testData.Values.AddRange(testSheet.Range("B7", "U7").Cells().Select(c => c.GetValue<string>()));
+            testData.Values.AddRange(testSheet.Range("B10", "U10").Cells().Select(c => c.GetValue<string>()));
+
+            return testData;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (saveTestingTemplateDialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            File.Copy("TestingTemplate.xlsx", saveTestingTemplateDialog.FileName, true);
+        }
+    }
+
+    class TestData
+    {
+        public string Student { get; set; }
+        public List<string> Values { get; set; }
+
+        public TestData()
+        {
+            Values = new List<string>();
+        }
+    }
+}
